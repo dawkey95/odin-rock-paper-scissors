4d549890ace9e810b9cc3a19f7ffe6803f7fdf21
Dawid Keyser <87341061+dawkey95@users.noreply.github.com>
2023-10-09 23:58:49 +1300

    Added comments to explain the code

--- app.js
+++ app.js
@@ -7,18 +7,23 @@ function game() {
 	let playerScore = 0;
 	let computerScore = 0;
 
-	let userChooseRounds = parseInt(
+	// player prompt to choose amount of rounds to be played
+	let playerChooseRounds = parseInt(
 		prompt('How many rounds would you like to play?')
 	);
 
-	// While loop to loop over game rounds
-	while (rounds !== userChooseRounds + 1) {
+	// While loop to loop over game rounds set by player
+	while (rounds !== playerChooseRounds + 1) {
+		// Computer choice by getting random number and using that number as the index
+		// on the array gameChoices.
 		function getComputerChoice() {
 			let index = Math.floor(Math.random() * (4 - 1) + 1) - 1;
 			let choice = gameChoices[index].toLowerCase();
 			return choice;
 		}
 
+		// Single round taking a playerChoice and computerChoice and then checking the conditions to win, lose or tie the round
+		// Whoever won the round get their score incremented
 		function playRound(playerChoice, computerChoice) {
 			if (playerChoice === 'rock' && computerChoice === 'scissors') {
 				playerScore += 1;
@@ -43,21 +48,25 @@ function game() {
 			}
 		}
 
+		// Players choice input which is made lowercase to avoid case-sensitivity
 		const playerChoice = prompt(
 			'Please type "Rock", "Paper" or "Scissors"'
 		).toLowerCase();
+
+		// computerChoice function is called
 		const computerChoice = getComputerChoice();
 
+		// Per round update
 		console.log(`Round ${rounds}:`);
 		console.log(`Player choice is: ${playerChoice}`);
 		console.log(`Computer choice is: ${computerChoice}`);
-
 		console.log(playRound(playerChoice, computerChoice));
 		console.log(`*****************************************`);
 
 		rounds++;
 	}
 
+	// Conditions are checked after the rounds are played with highest score winning the game
 	if (playerScore > computerScore) {
 		alert(
 			`You Won! You got ${playerScore} points and computer got ${computerScore} points.`
